cce55ba82ea429290e12af7015dc15d868ab7c21
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-05-29 22:34:38 -0400

    added param names

--- systems/composite.py
+++ systems/composite.py
@@ -5,7 +5,7 @@ from .system import System
 class CompositeSystem(System):
     
     def __init__(self, dt: float, systems: List[System]=[]) -> None:
-        super().__init__(dt, False, False)
+        super().__init__(dt, use_integral=False, use_derivative=False)
         self.systems = systems
 
     def _process(self, x: float, ix: float, dx: float) -> float:
